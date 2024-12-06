64dc465ec4db3af1936896fdcc13500956ce230f
1MuhireDavid <muhiredavid74@gmail.com>
2024-12-06 22:44:13 +0200

    add providers signin

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { signIn } from "next-auth/react";
 import Link from "next/link";
 
 const SignupPage = () => {
@@ -64,29 +63,6 @@ const SignupPage = () => {
             Sign Up
           </button>
         </form>
-
-        {/* Divider */}
-        <div className="my-6 flex items-center">
-          <hr className="flex-grow border-gray-600" />
-          <span className="mx-4 text-gray-400">OR</span>
-          <hr className="flex-grow border-gray-600" />
-        </div>
-
-        {/* GitHub OAuth Button */}
-        <button
-          onClick={() => signIn("github", { callbackUrl: "/api/dashboard" })}
-          className="flex items-center justify-center bg-gray-700 hover:bg-gray-600 text-white font-medium py-3 rounded-lg w-full transition"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            className="h-6 w-6 mr-2"
-          >
-            <path d="M12 .5C5.73.5.5 5.73.5 12c0 5.07 3.29 9.36 7.86 10.86.57.1.78-.25.78-.55v-2.08c-3.2.7-3.87-1.53-3.87-1.53-.52-1.32-1.27-1.67-1.27-1.67-1.03-.7.08-.68.08-.68 1.14.08 1.74 1.17 1.74 1.17 1.01 1.73 2.63 1.23 3.28.94.1-.73.4-1.23.72-1.51-2.55-.3-5.22-1.28-5.22-5.68 0-1.26.45-2.3 1.18-3.11-.12-.29-.52-1.48.11-3.08 0 0 .97-.31 3.18 1.19.92-.26 1.92-.39 2.91-.39s1.99.13 2.91.39c2.21-1.5 3.18-1.19 3.18-1.19.63 1.6.23 2.79.11 3.08.73.81 1.18 1.85 1.18 3.11 0 4.41-2.68 5.37-5.23 5.67.41.36.77 1.07.77 2.15v3.18c0 .3.21.65.79.55A10.5 10.5 0 0 0 23.5 12c0-6.27-5.23-11.5-11.5-11.5z" />
-          </svg>
-          Sign Up with GitHub
-        </button>
       </div>
     </div>
   );
